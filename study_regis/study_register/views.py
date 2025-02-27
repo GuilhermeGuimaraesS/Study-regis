9674c9e094ea150e6e43540b3006c2c9dab7d9f4
Guilherme Guimarães dos Santos <guilhermedossantos301@gmail.com>
2025-02-27 17:21:16 -0300

    Criação de página que mostra os tópicos cadastrados e adição de atalho no cabeçalho.

--- study_regis/study_register/views.py
+++ study_regis/study_register/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Topic
 
 def index(request):
     return render(request, 'study_register/index.html')
+
+
+def topics(request):
+    """Mostra todos os assuntos"""
+    topics = Topic.objects.order_by('date_added') # Pega os tópicos contidos no banco de dados
+    context = {'topics': topics} # Dicionário contendo os tópicos que foram obtidos do banco de dados
+    return render(request,'study_register/topics.html', context)
 
