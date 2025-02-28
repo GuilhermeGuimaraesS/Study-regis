1f16c86a5d89f63b59a70bbe900186f073b92106
Guilherme Guimarães dos Santos <guilhermedossantos301@gmail.com>
2025-02-28 17:05:10 -0300

    Criação de uma página para listar todos os tópicos cadastrados e uma para mostrar anotações referentes à um tópico escolhido.

--- study_regis/study_register/views.py
+++ study_regis/study_register/views.py
@@ -6,8 +6,16 @@ def index(request):
 
 
 def topics(request):
-    """Mostra todos os assuntos"""
+    """Mostra todos os temas de resumos."""
     topics = Topic.objects.order_by('date_added') # Pega os tópicos contidos no banco de dados
     context = {'topics': topics} # Dicionário contendo os tópicos que foram obtidos do banco de dados
     return render(request,'study_register/topics.html', context)
+
+
+def topic(request, topic_id):
+    """Mostra os resumos de um tema escolhido."""
+    topic = Topic.objects.get(id = topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'study_register/topic.html', context)
 
